1ffeec213112ae650f2d022106559a44076ce5e2
rubyruins <soumya.parekh@somaiya.edu>
2021-02-08 00:57:01 +0530

    updated app

--- dash.py
+++ dash.py
@@ -15,14 +15,14 @@ import zipfile
 
 # @st.cache(persist = True, suppress_st_warning = True)
 def load_data():
-	with open('Data/articles.pkl', 'rb') as f:
+	with open('./Data/articles.pkl', 'rb') as f:
 		main_data = pickle.load(f)
-	with zipfile.ZipFile('Data/knn_model.zip', 'r') as zip_ref:
-		zip_ref.extractall('Data/knn/model')
-	with zipfile.ZipFile('Data/knn_vectorizer.zip', 'r') as zip_ref:
-		zip_ref.extractall('Data/knn/vectorizer')
-	loaded_model = pickle.load(open('Data/knn/model/knn_model.p','rb'))
-	loaded_vectorizer = pickle.load(open('Data/knn/vectorizer/knn_vectorizer.p','rb'))
+	with zipfile.ZipFile('./Data/knn_model.zip', 'r') as zip_ref:
+		zip_ref.extractall('./Data/knn/model')
+	with zipfile.ZipFile('./Data/knn_vectorizer.zip', 'r') as zip_ref:
+		zip_ref.extractall('./Data/knn/vectorizer')
+	loaded_model = pickle.load(open('./Data/knn/model/knn_model.p','rb'))
+	loaded_vectorizer = pickle.load(open('./Data/knn/vectorizer/knn_vectorizer.p','rb'))
 	return main_data, loaded_model, loaded_vectorizer
 
 st.title("Journalist Matching.")
